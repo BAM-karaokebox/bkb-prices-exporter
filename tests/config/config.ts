95d50b77c6d438c40c31a72c4f2545dec62fee34
Laurent Malvert <66669996+laurent-malvert-freelance@users.noreply.github.com>
2023-10-11 14:10:46 +0200

    Inject monitoring marker for GTM detection / GA4 exclusion

--- tests/config/config.ts
+++ tests/config/config.ts
@@ -1,6 +1,6 @@
 export const BASE_URL = "https://fr.bam-karaokebox.com";
 
-export const QA_MARKER = "?utm_source=bkb-website-tests&utm_medium=qa-bot&utm_campaign=monitoring";
+export const QA_MARKER = "?utm_source=bkb-website-tests&utm_medium=qa-bot&utm_campaign=monitoring&traffic_type=monitoring";
 
 export const START_DATE = new Date();
 
